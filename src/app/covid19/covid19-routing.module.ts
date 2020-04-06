04e4dd135569a2ccac865eb9804a5a50a47e1a12
chandran lakshmanan <chandran.lakshmanan@actian.com>
2020-04-06 11:18:34 +0530

    Country wise data

--- src/app/covid19/covid19-routing.module.ts
+++ src/app/covid19/covid19-routing.module.ts
@@ -5,7 +5,7 @@ import { ViewbyCountryComponent } from './viewby-country/viewby-country.componen
 import { SearchFilterPipe } from './search.pipe';
 import { Covid19IndiaComponent } from './covid19-india/covid19-india.component';
 
-const routes: Routes = [{ path: '', component: Covid19Component }, { path: 'covid19-india', component: Covid19IndiaComponent }, { path: 'covid19/country/:country', component: ViewbyCountryComponent }];
+const routes: Routes = [{ path: '', component: Covid19Component }, { path: 'covid19-india', component: Covid19IndiaComponent }, { path: 'country/:country', component: ViewbyCountryComponent }];
 export const components = [Covid19Component, ViewbyCountryComponent, SearchFilterPipe, Covid19IndiaComponent];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
